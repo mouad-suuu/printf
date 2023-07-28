5d53e8944b6ece343329d59d0779196bb028eac6
NabilM5 <nabil.mouhamech@usmba.ac.ma>
2023-07-27 17:39:00 -0700

    update

--- handlers.c
+++ handlers.c
@@ -160,8 +160,6 @@ int handle_precision(va_list args, const char *modifier, char *index)
 /**
  * handle_specifiers - Matches a conversion specifier with
  *                     a corresponding conversion function.
- * @specifier: A pointer to a potential conversion specifier.
- *
  * Return: If a conversion function is matched - a pointer to the function.
  *         Otherwise - NULL.
  */
